3454c32dcef85b136b359ce0754103188ea10de3
yahavdan <yahavmdan@gmail.com>
2024-05-20 17:35:57 +0300

    storage things

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,8 +38,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app' ),
-            'url' => env('APP_URL').'/storage',
+            'root' => storage_path('app'),
+            'url' => env('APP_URL'),
             'visibility' => 'public',
             'throw' => false,
         ],
